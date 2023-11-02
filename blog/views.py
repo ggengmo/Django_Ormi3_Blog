13b996a409c14c4e39fa20c28cf0606cc97e23ad
ggengmo <rudah365dlf@gmail.com>
2023-11-02 17:37:46 +0900

    post_list UI 수정 완료

--- blog/views.py
+++ blog/views.py
@@ -51,7 +51,7 @@ class PostDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['top_level_comments'] = self.object.comments.filter(nested_reply__isnull=True).order_by('-created_at')
+        context['top_comments'] = self.object.comments.filter(nested_reply__isnull=True).order_by('-created_at')
         context['comment_form'] = CommentForm()
         return context
     
